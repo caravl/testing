e7c419471237b2241fa15b331a4ecf6b836085ff
Armaan Gupta <armaan@thedialects.com>
2019-07-31 13:57:10 -0400

    Initial Commit

---
+++ public/assets/js/thirdparty.js
@@ -0,0 +1,9 @@
+
+/* **********************************************
+     Begin thirdparty.js
+********************************************** */
+
+$(document).ready(function() {
+    var main_route = (window.location.pathname.split("/")[1]);
+    $('#navbar_' + main_route).addClass('active');
+});
