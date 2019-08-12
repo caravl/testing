fe86de3a2937e0a7b413175eb164e9b84e64625b
Armaan Gupta <armaan@thedialects.com>
2019-08-12 12:50:00 -0400

    Update for new main page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :account, :controller => 'account'
   resources :brands, :controller => 'brands'
+  resources :workplaces, :controller => 'workplaces'
   root 'welcome#index'
   resources :welcome, only: [:create]
 
